44a0d48d068f6ffb3afe8bb257e30ca8baaffbb9
mizzpika <148228003+mizzpika@users.noreply.github.com>
2024-06-10 19:37:04 +0900

    Update warshall_floyd.cpp

--- graph/warshall_floyd.cpp
+++ graph/warshall_floyd.cpp
@@ -1,8 +1,8 @@
 template<typename T>void warshall_floyd(vector<vector<T>> &g){
     const auto INF = numeric_limits<T>::max();
-    for(int k = 0; k < g.size(); ++k){
-        for(int i = 0; i < g.size(); ++i){
-            for(int j = 0; j < g.size(); ++j){
+    for(ll k = 0; k < g.size(); ++k){
+        for(ll i = 0; i < g.size(); ++i){
+            for(ll j = 0; j < g.size(); ++j){
                 if(g[i][k] == INF || g[k][j] == INF)continue;
                 chmin(g[i][j] , g[i][k] + g[k][j]);
             }
