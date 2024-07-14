220f8d409b7bf5684e542a7c65d5c763510041f7
mizzpika <148228003+mizzpika@users.noreply.github.com>
2024-07-14 21:18:02 +0900

    Update graph_template.cpp

--- graph/graph_template.cpp
+++ graph/graph_template.cpp
@@ -1,11 +1,34 @@
-template<typename T>struct edge{
-    ll src, to; T cost;
-    edge(ll to, T cost) : src(-1), to(to), cost(cost){}
-    edge(ll src, ll to, T cost) : src(src), to(to), cost(cost){}
-    edge &operator=(const ll &x){
-        to = x;
-        return *this;
+//graph_template
+struct Edge{
+    ll to;
+    ll cost;
+    Edge(ll to, ll cost) : to(to), cost(cost) {}
+    bool operator>(const Edge &e) const{
+        return cost > e.cost;
     }
-    operator ll() const {return to;}
 };
-template<typename T>using graph = vector<vector<edge<T>>>;
+
+struct Graph{
+    vector<vector<Edge>> G;
+
+    Graph(ll N){
+        G.resize(N);
+    }
+
+    ll size(){
+        return G.size();
+    }
+
+    void add_edge(ll from, ll to, ll cost = 1){
+        G[from].push_back(Edge(to, cost));
+        G[to].push_back(Edge(from, cost));
+    }
+
+    void add_directed_edge(ll from, ll to, ll cost = 1){
+        G[from].push_back(Edge(to, cost));
+    }
+
+    vector<Edge> &operator[](ll v){
+        return G[v];
+    }
+};
