7863a5526fdfb7cb9a604f6ad0b5889cf010f0e4
mizzpika <148228003+mizzpika@users.noreply.github.com>
2024-07-14 21:18:38 +0900

    Update bellman_ford.cpp

--- graph/bellman_ford.cpp
+++ graph/bellman_ford.cpp
@@ -1,16 +1,72 @@
-template<typename T>vector<T> bellman_ford(edges<T> &edges, int V, int s){
-    const auto INF = numeric_limits<T>::max();
-    vector<T>dist(V, INF);
-    dist[s] = 0;
-    for(ll i = 0; i < V - 1; ++i){
-        for(auto &e : edges){
-            if(dist[e.src] == INF)continue;
-            dist[e.to] = min(dist[e.to], dist[e.src] + e.cost);
+//bellman_ford
+struct bellman_ford{
+    vector<ll> dist;
+    vector<ll> prev;
+    ll start;
+    ll size;
+    bool cl = false;
+
+    //bellman_fordを構築
+    bellman_ford(Graph &g, ll s){
+        start = s;
+        ll n = g.size();
+        size = n;
+        dist.assign(n, 1LL << 60);
+        prev.assign(n, -1);
+        vector<ll> counts(n);
+        vector<bool> inqueue(n);
+
+        queue<ll> q;
+        dist[s] = 0;
+        q.push(s);
+        inqueue[s] = true;
+
+        while(!q.empty()){
+            ll from = q.front();
+            q.pop();
+            inqueue[from] = false;
+
+            for(auto &e : g[from]){
+                ll d = dist[from] + e.cost;
+                if(d < dist[e.to]){
+                    dist[e.to] = d;
+                    prev[e.to] = from;
+                    if(!inqueue[e.to]){
+                        q.push(e.to);
+                        inqueue[e.to] = true;
+                        ++counts[e.to];
+                        
+                        if(n < counts[e.to])cl = true;
+                    }
+                }
+            }
         }
     }
-    for(auto &e : edges){
-        if(dist[e.src] == INF)continue;
-        if(dist[e.src] + e.cost < dist[e.to])return vector<T>();
+
+    //最小コストを求める
+    ll get_cost(ll to){
+        return dist[to];
     }
-    return dist;
-}
+
+    //最短経路を求める
+    vector<ll> get_path(ll to){
+        vector<ll> path;
+        if(dist[to] != 1LL << 60){
+            for(ll i = to; i != -1; i = prev[i]){
+                path.push_back(i);
+            }
+            reverse(path.begin(), path.end());
+        }
+        return path;
+    }
+
+    //到達可能か調べる
+    bool cango(ll to){
+        return (dist[to] != 1LL << 60);
+    }
+
+    //負の閉路の有無を調べる
+    bool closed(){
+        return cl;
+    }
+};
