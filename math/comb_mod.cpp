633383c4bd48cdd1868dc8038b106730ffb76900
mizzpika <148228003+mizzpika@users.noreply.github.com>
2024-07-14 21:47:58 +0900

    Update comb_mod.cpp

--- math/comb_mod.cpp
+++ math/comb_mod.cpp
@@ -1,19 +1,28 @@
-vector<long long> fact, fact_inv, inv;
-void init_comb_mod(int SIZE, int MOD) {
-    fact.resize(SIZE + 5);
-    fact_inv.resize(SIZE + 5);
-    inv.resize(SIZE + 5);
-    fact[0] = fact[1] = 1;
-    fact_inv[0] = fact_inv[1] = 1;
+// comb_mod
+using mint_comb = atcoder::static_modint<1000000007>;
+
+vector<mint_comb> fac, finv, inv;
+
+// テーブルを作る前処理
+void init_comb_mod(ll size) {
+    size += 109;
+    fac.resize(size);
+    finv.resize(size);
+    inv.resize(size);
+    const ll MOD = mint_comb::mod();
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
     inv[1] = 1;
-    for (int i = 2; i < SIZE + 5; i++) {
-        fact[i] = fact[i - 1] * i % MOD;
-        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
-        fact_inv[i] = fact_inv[i - 1] * inv[i] % MOD;
+    for (ll i = 2; i < size; i++){
+        fac[i] = fac[i - 1] * i;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i);
+        finv[i] = finv[i - 1] * inv[i];
     }
 }
-long long comb_mod(int n, int k, int MOD) {
-    assert(!(n < k));
-    assert(!(n < 0 || k < 0));
-    return fact[n] * (fact_inv[k] * fact_inv[n - k] % MOD) % MOD;
+
+// 二項係数計算
+mint_comb comb_mod(ll n, ll k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * finv[k] * finv[n - k];
 }
