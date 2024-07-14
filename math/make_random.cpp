7972efd392e25421cd51d78161abf0b3b3836027
mizzpika <148228003+mizzpika@users.noreply.github.com>
2024-07-14 21:47:29 +0900

    Update make_random.cpp

--- math/make_random.cpp
+++ math/make_random.cpp
@@ -1,6 +1,20 @@
-ll make_random(ll l, ll r){
+//make_random
+//long long
+ll rand_ll(ll l, ll r){
+    if(l > r)swap(l, r);
     random_device rd;
     mt19937_64 gen(rd());
     uniform_int_distribution<ll>dis(l, r);
     return dis(gen);
+}
+
+//int
+int rand_int(int l, int r){
+    if(l > r)swap(l, r);
+    return l + rand() % (r - l + 1);
+}
+
+//double
+ld rand_double(){
+    return (ld)1.0 * rand() / RAND_MAX;
 }
