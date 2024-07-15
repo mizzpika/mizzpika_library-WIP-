2a799b7be46b38199a5c6f9ce339be23d48b7784
mizzpika <148228003+mizzpika@users.noreply.github.com>
2024-07-15 19:12:42 +0900

    Create tree_diameter.cpp

---
+++ graph/tree_diameter.cpp
@@ -0,0 +1,22 @@
+//木の直径を求める
+ll tree_diameter(Graph g){
+    ll n = g.size();
+    dijkstra d(g, 0);
+    ll mx = 0, idx = -1;
+    for(ll i = 0; i < n; i++){
+        if(mx < d.get_cost(i)){
+            mx = d.get_cost(i);
+            idx = i;
+        }
+    }
+
+    dijkstra d2(g, idx);
+    ll mx2 = 0;
+    for(ll i = 0; i < n; i++){
+        if(mx2 < d2.get_cost(i)){
+            mx2 = d.get_cost(i);
+        }
+    }
+
+    return mx2;
+}
